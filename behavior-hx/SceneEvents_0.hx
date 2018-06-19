9d5cdbd35853de32897450d317e643aabda2aca0
Olek <trepala.aleksander@gmail.com>
2018-06-19 20:20:43 +0200

    Resized player and enemies to match new map size.

--- behavior-hx/SceneEvents_0.hx
+++ behavior-hx/SceneEvents_0.hx
@@ -40,6 +40,7 @@ import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Body;
 import box2D.dynamics.B2Fixture;
 import box2D.dynamics.joints.B2Joint;
+import box2D.collision.shapes.B2Shape;
 
 import motion.Actuate;
 import motion.easing.Back;
@@ -69,16 +70,13 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class ActorEvents_5 extends ActorScript
+class SceneEvents_0 extends SceneScript
 {
-	public var _Points:Float;
 	
 	
-	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	public function new(dummy:Int, dummy2:Engine)
 	{
-		super(actor);
-		nameMap.set("Points", "_Points");
-		_Points = 5.0;
+		super();
 		
 	}
 	
